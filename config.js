edd4aca729a13c473c806c1b7aa704a389f32ab7
Yishak Amdeselassie <63883719+Isaacbit@users.noreply.github.com>
2022-04-04 14:14:51 +0300

    Update config.js

--- config.js
+++ config.js
@@ -12,9 +12,9 @@ chapters: [
         {
             id: 'first',
             alignment: 'left',
-            title: 'Enabling Educational Equity in Ethiopia with MapBox',
+            title: 'Current State of Connectivity in Ethiopia',
             image: '',
-            description: 'Enabling Educational Equity with MapBox in Ethiopia ',
+            description: 'Today access to quality education remains limited for the majority of the country. Despite advancements by the Ministry of Education to equip Ethiopian youth with digital, open resources; most Ethiopians still remain in the dark with wifi penetration at only 15%.',
             location: {
 		center: [11.56910, 1.96241],
 		zoom: 2.48,
@@ -77,9 +77,9 @@ chapters: [
         {
             id: 'phl',
             alignment: 'left',
-            title: 'Current State in Ethiopia + Problem Statement',
+            title: 'Redefining Educational Equity Through Connectivity',
             image: '',
-            description: 'Today in Ethiopia access to quality education remains limited and a luxury to those who have access to the internet. Despite advancements by the Ministry of Education to enable Ethiopian youth with digital, open resources; most of the country still remains in the dark. With wifi penetration at only',
+            description: 'The ongoing global pandemic has redefined how students learn from the classroom over the world. Increased connectivity cements e-learning at the forefront of Africa’s digital transformation and serves as a key component to ensuring learning continues despite instability. ',
             location: {
 				center: [39.00225, 9.04662],
 				zoom: 5.18,
@@ -141,9 +141,9 @@ chapters: [
         {
             id: 'bike-lanes',
             alignment: 'right',
-            title: 'Connectivity Insights / Research Findings',
+            title: 'Bridging the Connectivity Gap',
             image: '',
-            description: 'How many students in ethiopia currently use e-learning? What projections can be made on how can this number can expand with increased connectivity? What does educational inequity driven by lack of connectivity in Ethiopia look like? (need stats)',
+            description: 'The Ministry of Education is planning to connect all schools in Ethiopia with an educational backbone to access e-learning services in their central data center in Addis Ababa and for interconnectivity between the schools. <br> Expanding connectivity in Ethiopia protects the right to schooling for students regardless of socioeconomic status or geographic location.',
             location: {
 				center: [38.45382, 7.53192],
 				zoom: 5.79,
@@ -198,9 +198,9 @@ chapters: [
         {
             id: 'indego',
             alignment: 'left',
-            title: 'Proposal + The Solution',
+            title: 'The Solution',
             image: '',
-            description: 'Iceaddis created this proposal for supplying a crucial component in this endeavor. With K3 Telecom technology it is intended to bridge the last mile to the schools from the EEP towers and provide high speed connections to the Data center with a guaranteed bandwidth of minimum 300 Mbit per school or receiver.',
+            description: 'iceaddis created this proposal for supplying a crucial component in this endeavor. With K3 Telecom technology it is intended to bridge the last mile to the schools from the EEP towers and provide high speed connections to the Data center with a guaranteed bandwidth of minimum 300 Mbit per school or receiver.',
             location: {
 				center: [36.54895, 6.46459],
 				zoom: 6.55,
